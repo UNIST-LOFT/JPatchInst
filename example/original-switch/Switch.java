06349ca54bf70849d4ad2d5229a3b5d98e247839
FreddyYJ <kyj1411@unist.ac.kr>
2023-03-09 02:57:25 +0000

    Add switch examples

---
+++ example/original-switch/Switch.java
@@ -0,0 +1,25 @@
+public class Switch {
+    public void switchTest(int a) {
+        int b=a;
+        if (b==1)
+            System.out.println("b is 1!");
+        else if (b==2)
+            System.out.println("b is 2...");
+        else
+            System.out.println("b is not 1 or 2?");
+
+        switch (a) {
+            case 1:
+                System.out.println("a is 1!");
+                break;
+            case 2:
+                System.out.println("a is 2...");
+                break;
+            default:
+                System.out.println("a is not 1 or 2?");
+                break;
+        }
+
+        System.out.println("a is " + Integer.toString(a));
+    }
+}
