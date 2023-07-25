cb18d7b570f5a1021bc292bda94cc85668c051f5
FreddyYJ <kyj1411@unist.ac.kr>
2023-07-25 03:16:02 +0000

    Add Javadoc to update codes

--- src/main/java/kr/ac/unist/apr/GlobalStates.java
+++ src/main/java/kr/ac/unist/apr/GlobalStates.java
@@ -37,24 +37,57 @@ public class GlobalStates {
   
   /** Full name of this class */
   public static final String STATE_CLASS_NAME="kr.ac.unist.apr.GlobalStates";
+  /** Name of initializer */
   public static final String STATE_INIT="initialize";
   
+  /** Name of variable to check initialized */
   public static final String STATE_IS_INITIALIZED="isInitialized";
+  /** Name of variable of previous ID */
   public static final String STATE_PREV_ID="previousId";
+  /** Name of variable of map */
   public static final String STATE_BRANCH_COUNT="branchCount";
 
+  /** Name of environment variable to record covered branch or not */
   public static final String STATE_ENV_RECORD="GREYBOX_BRANCH";
   
   /**
    * The executed branch ID in previous. Used for calculate current ID.
    */
   public static int previousId=0;
+  /**
+   * The number of execution of each branch.
+   * <p>
+   *  Maximum number of branch is 200000.
+   * </p>
+   */
   public static int[] branchCount=new int[200000]; // Make enough size of array to reduce the overhead.
   
+  /**
+   * Flag to check this class is initialized or not.
+   */
   public static boolean isInitialized=false;
 
+  /**
+   * File writer to save the map.
+   */
   public static FileWriter resultFile=null;
 
+  /**
+   * Initialize this class.
+   * <p>
+   *  This method should be called once before run the instrumented code.
+   * 
+   *  This method adds shutdown hook to save the map to the file.
+   *  File name should be defined in environment variable GREYBOX_RESULT.
+   * 
+   *  How to use this method:
+   *  <pre>
+   *    if (!GlobalStates.isInitialized)
+   *      GlobalStates.initialize();
+   *  </pre>
+   *  This will update the flag isInitialized to true.
+   * </p>
+   */
   public static void initialize() {
     if (System.getenv("GREYBOX_BRANCH").equals("1")) {
       Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
